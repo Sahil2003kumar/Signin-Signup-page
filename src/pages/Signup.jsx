4ce4ab20f82696d9a1f664a68cac25c9ddf4501d
Sahil Kumar <sahil12344kumar@gmail.com>
2025-11-02 22:23:12 +0530

    Backend folder

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 const Signup = () => {
+    const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false)
+
+    const [fullName, setFullName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <div className='min-h-screen w-full flex items-center justify-center p-4 bg-teal-50'>
 
@@ -12,20 +18,20 @@ const Signup = () => {
                 {/* fullname */}
                 <div className='mb-4'>
                     <label htmlFor="fullName" className='block text-gray-700 font-medium mb-1'>Full Name</label>
-                    <input type="text" className='w-full border rounded-lg px-3 py-2 border-teal-500 outline-none text-teal-600' placeholder='Enter Your Full Name' />
+                    <input onChange={(e)=>setFullName(e.target.value)} value={fullName} type="text" className='w-full border rounded-lg px-3 py-2 border-teal-500 outline-none text-teal-600' placeholder='Enter Your Full Name' />
                 </div>
 
                 {/* email */}
                 <div className='mb-4'>
                     <label htmlFor="email" className='block text-gray-700 font-medium mb-1'>Email</label>
-                    <input type="email" className='w-full border rounded-lg px-3 py-2 border-teal-500 outline-none text-teal-700' placeholder='Enter Your Email' />
+                    <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" className='w-full border rounded-lg px-3 py-2 border-teal-500 outline-none text-teal-700' placeholder='Enter Your Email' />
                 </div>
 
                 {/* Password */}
                 <div className='mb-4'>
                     <label htmlFor="password" className='block text-gray-700 font-medium mb-1'>Password</label>
                     <div className='relative'>
-                        <input type={`${showPassword ? "text" : "password"}`} className='w-full border rounded-lg px-3 py-2 border-teal-500 outline-none text-teal-600' placeholder='Enter Your Password' />
+                        <input onChange={(e)=>setPassword(e.target.value)} value={password} type={`${showPassword ? "text" : "password"}`} className='w-full border rounded-lg px-3 py-2 border-teal-500 outline-none text-teal-600' placeholder='Enter Your Password' />
                         <button className='absolute cursor-pointer right-3 top-2.5 text-teal-700' onClick={() => setShowPassword(prev => !prev)}>{!showPassword ? <FaEye size={18} /> : <FaEyeSlash size={18} />}</button>
 
                     </div>
