ce001a80d620569486602b02db5caa2cc1ff5d7e
Sahil Kumar <sahil12344kumar@gmail.com>
2025-11-03 11:52:38 +0530

    signout controller

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -66,3 +66,14 @@ return res.status(400).json({message:"incorrect password and Email"})
         return res.status(500).json({ message: `signIn error${error}` })
     }
 }
+
+//signout controlller 
+export const signOut = async(req, res)=>{
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({message:"logout successfully"})
+    } catch (error) {
+        
+        return res.status(200).json({message:`signout error ${error}`})
+    }
+}
